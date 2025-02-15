5dcb058983bcdfb861eb137f817f8c2f24c9f1c8
Maxim <user@fhmt0bq1qm82avkhdjsh.auto.internal>
2025-02-15 07:16:49 +0000

    Add compose file
      Bugfix in up.sh, add app compose file

--- up.sh
+++ up.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "stop" ]; then
 	echo "Start destroy an application..."
-	docker compose -f app.compose.yml down
+	docker compose -f app.compose.yml -p rbmdkrfinalapp down
 else
 	echo "Start create an application..."
-	docker compose -f app.compose.yml up -d
+	docker compose -f app.compose.yml -p rbmdkrfinalapp up -d 
 fi
 
 echo "...complete"
