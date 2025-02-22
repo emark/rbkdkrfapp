03bee7a3621c5df717fccb76bdebeecd5adf7277
Maxim <user@epdthan2oetj62tlej8f.auto.internal>
2025-02-22 10:41:33 +0000

    Fix running containers

--- up.sh
+++ up.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "stop" ]; then
 	echo "Start destroy an application..."
-	docker compose -f efk.compose.yml -p rbmdkrfinalefk down
 	docker compose -f app.compose.yml -p rbmdkrfinalapp down
+	docker compose -f efk.compose.yml -p rbmdkrfinalefk down
 else
 	echo "Start create an application..."
 	docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
