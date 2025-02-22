5012719216a421b27e52c78fb1298a11869bdab7
Maxim <user@epdthan2oetj62tlej8f.auto.internal>
2025-02-22 10:35:22 +0000

    q

--- up.sh
+++ up.sh
@@ -2,9 +2,11 @@
 
 if [ "$1" = "stop" ]; then
 	echo "Start destroy an application..."
+	docker compose -f efk.compose.yml -p rbmdkrfinalefk down
 	docker compose -f app.compose.yml -p rbmdkrfinalapp down
 else
 	echo "Start create an application..."
+	docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
 	docker compose -f app.compose.yml -p rbmdkrfinalapp up -d
 fi
 
