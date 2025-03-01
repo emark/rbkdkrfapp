7908b18c993dfa1d65f4070502239ecef5fac7d6
Maxim <you@example.com>
2025-03-01 15:36:12 +0000

    Add EFK stack

--- up.sh
+++ up.sh
@@ -3,10 +3,10 @@
 if [ "$1" = "stop" ]; then
 	echo "Start destroy an application..."
 	docker compose -f app.compose.yml -p rbmdkrfinalapp down
-#	docker compose -f efk.compose.yml -p rbmdkrfinalefk down
+	docker compose -f efk.compose.yml -p rbmdkrfinalefk down
 else
 	echo "Start create an application..."
-#	docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
+	docker compose -f efk.compose.yml -p rbmdkrfinalefk up -d
 	docker compose -f app.compose.yml -p rbmdkrfinalapp up -d
 fi
 
